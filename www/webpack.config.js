dc0d1190942042f8fccfc208c4af18ebd798c4f8
Robin Lake <robin@tempus-ex.com>
2022-12-08 09:04:32 -0800

    added npm project

---
+++ www/webpack.config.js
@@ -0,0 +1,33 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.ts",
+  // entry: "./bootstrap.js",
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.wasm'],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin(['index.html'])
+  ],
+  experiments: {
+    asyncWebAssembly: true,
+    lazyCompilation: true,
+    syncWebAssembly: true,
+  },
+};
